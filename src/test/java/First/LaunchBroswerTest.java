fc2391a47d34d736a10e9ba5f16271bf05fc294b
Amrutha <sindhu10.ammu@gmail.com>
2024-04-20 10:52:11 +0530

    first commit

---
+++ src/test/java/First/LaunchBroswerTest.java
@@ -0,0 +1,13 @@
+package First;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBroswerTest {
+	
+	@Test
+	public void launch() {
+		WebDriver driver = new ChromeDriver();
+	}
+}
